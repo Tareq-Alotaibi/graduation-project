e0d7cacc7d8cd71254bd5e9a6983238d3a21a699
Tareq Alotaibi <tareq@gmail.com>
2023-01-02 23:56:32 +0300

    time fixes

--- parking/form.py
+++ parking/form.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from parking.models import Reservation
 from datetime import datetime
+from datetime import timedelta
 
 
 class ReservationForm(ModelForm):
@@ -10,15 +11,23 @@ class ReservationForm(ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        format = "%H:%M:%S"
+        format = "%I:%M %p"
+
         start = cleaned_data['checkin'].strftime(format)
         end =   cleaned_data['checkout'].strftime(format)
+        
+        date_start = datetime.strptime(start, format)
+        date_end = datetime.strptime(end, format)
+
+        if 0 <= cleaned_data['checkout'].hour < 12:
+            date_end = datetime.strptime(end, format) + timedelta(days=1)
+        
         # Get interval between two timstamps as timedelta object
-        diff = datetime.strptime(end, format) - datetime.strptime(start, format)
+        diff = date_end - date_start
         # Get interval between two timstamps in hours
         duration_in_s = diff.total_seconds()
 
-        hours = divmod(duration_in_s, 3600)[0]
+        hours = duration_in_s / (60 * 60)
 
         if hours < 1:
             msg = "Checkout must be at least 1 hour more than checkin."
